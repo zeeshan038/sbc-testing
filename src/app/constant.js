e5bacd47b007d38ea25bf0b7bb725e66ba32ed35
zeeshan038 <hankhan123623@gmail.com>
2026-03-13 21:50:10 +0500

    fix

--- src/app/constant.js
+++ src/app/constant.js
@@ -1,3 +1,4 @@
-export const BASE_URL = "http://localhost:8080";
+// export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://sendbycloud-backend.onrender.com";
 
 export const auth_url = "/api/user"
